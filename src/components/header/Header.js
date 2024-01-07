8370a5ba1ae6f7329392c20aa93dfaab0717b31b
payose <inipayose@gmial.com>
2024-01-07 17:03:23 +0100

    testing

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -11,16 +11,16 @@ const Header = () => {
     <Navbar bg='dark' variant='dark' expand='lg'>
       <Container fluid>
         <Navbar.Brand href="/" style={{"color": 'gold'}}>
-          <FontAwesomeIcon icon={faVideoSlash}> </FontAwesomeIcon>Gold
+            <FontAwesomeIcon icon={faVideoSlash}> </FontAwesomeIcon>Gold
         </Navbar.Brand>
         <Navbar.Toggle aria-controls='navbarScroll'></Navbar.Toggle>
         <Navbar.Collapse id="navbarScroll">
-          <Nav className='me-auto my-2 my-lg-0' style={{maxHeight: '100px' }} navbarScroll>
-          <NavLink className="nav-link" to='/'>Home</NavLink>
-          <NavLink className="nav-link" to='/watchList'>Watch List</NavLink>
-          </Nav>
-          <Button variant="outline-info" className="me-2">SignIn</Button>
-          <Button variant="outline-info">SignUp</Button>
+            <Nav className='me-auto my-2 my-lg-0' style={{maxHeight: '100px' }} navbarScroll>
+            <NavLink className="nav-link" to='/'>Home</NavLink>
+            <NavLink className="nav-link" to='/watchList'>Watch List</NavLink>
+            </Nav>
+            <Button variant="outline-info" className="me-2">SignIn</Button>
+            <Button variant="outline-info">SignUp</Button>
         </Navbar.Collapse>
       </Container>
     </Navbar>
